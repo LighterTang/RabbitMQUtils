fc26c6ae4a1d5d60d270621f188771c764a2f893
tangquanbin <1269106963@qq.com>
2019-04-21 23:02:05 +0800

    topic

---
+++ rabbitmq_base/src/main/java/com/mq/mqutils/tutorial/pubsub/TopicLogRecv2.java
@@ -0,0 +1,42 @@
+package com.mq.mqutils.tutorial.pubsub;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * @Title: TopicLogRecv1
+ * @Package: com.mq.mqutils.tutorial.pubsub
+ * @Description: TODO（添加描述）
+ * @Author: tangquanbin
+ * @Data: 2019/4/21 0021 22:51
+ * @Version: V1.0
+ */
+public class TopicLogRecv2 {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.1.4");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        //# (hash) can substitute for zero or more words.
+        channel.queueBind(queueName, EXCHANGE_NAME, "#.info");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
---
+++ rabbitmq_base/src/main/java/com/mq/mqutils/tutorial/pubsub/TopicLogSend.java
@@ -0,0 +1,47 @@
+package com.mq.mqutils.tutorial.pubsub;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @Title: TopicLogSend
+ * @Package: com.mq.mqutils.tutorial.pubsub
+ * @Description: TODO（添加描述）
+ * @Author: tangquanbin
+ * @Data: 2019/4/21 0021 22:49
+ * @Version: V1.0
+ */
+public class TopicLogSend {
+    private static final String EXCHANGE_NAME = "topic_logs";
+    private static final List<String> routingKeys = Arrays.asList("corn.error", "corn.info","auth.info","auth.error","auth.test.info");
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.1.4");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+            for (int i = 0; i < 10; i++) {
+                String routingKey = routingKeys.get(new Random().nextInt(5));
+                String message = routingKey + "-" + i;
+
+                channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+            }
+
+
+        }
+    }
+
+}

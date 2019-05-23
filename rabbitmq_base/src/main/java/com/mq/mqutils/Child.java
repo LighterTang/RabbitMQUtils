9da15095521ff778853453ed8e4cda87aa3b8fc0
tangquanbin <1269106963@qq.com>
2019-05-23 20:23:47 +0800

    pic

---
+++ rabbitmq_base/src/main/java/com/mq/mqutils/Child.java
@@ -0,0 +1,23 @@
+package com.mq.mqutils;
+
+/**
+ * @Title: Child
+ * @Package: com.mq.mqutils
+ * @Description: TODO（添加描述）
+ * @Author: tangquanbin
+ * @Data: 2019/5/21 0021 22:54
+ * @Version: V1.0
+ */
+public class Child extends Parent {
+
+    @Override
+    public void print1() {
+        super.print1();
+        System.out.println();
+    }
+
+    @Override
+    public void print3() {
+
+    }
+}
